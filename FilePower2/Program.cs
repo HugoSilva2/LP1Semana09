2d22c7d367a535241edc95f2d1fd3275111da1d3
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-05-10 16:09:14 +0100

    Ẽvery Change Done

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using System.IO;
 
-namespace FilePower1
+namespace FilePower2
 {
     class Program
     {
         static void Main(string[] filename)
         {  
+            string path = $"FilePower1/{filename[0]}";
+
+            using StreamWriter something = new StreamWriter(path);
+
             string input = Console.ReadLine();
-
-            Queue<string> q = new Queue<string>();
-
             while (input != "")
             {
-                q.Enqueue(input);
+                something.WriteLine(input);
                 input = Console.ReadLine();
             }
-            File.WriteAllLines($"FilePower1/{filename[0]}", q);
+            something.Close();
         }
     }
 }
