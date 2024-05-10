837b7fd8e792868461f8e60ec6dc705f51eee707
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-05-10 16:03:51 +0100

    Added FilePower1 stuff to FilePower2

--- FilePower2/Program.cs
+++ FilePower2/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace FilePower2
+namespace FilePower1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        static void Main(string[] filename)
+        {  
+            string input = Console.ReadLine();
+
+            Queue<string> q = new Queue<string>();
+
+            while (input != "")
+            {
+                q.Enqueue(input);
+                input = Console.ReadLine();
+            }
+            File.WriteAllLines($"FilePower1/{filename[0]}", q);
         }
     }
 }
