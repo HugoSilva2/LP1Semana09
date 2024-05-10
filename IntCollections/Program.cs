08d543e9f69cecebae4fc84e07a49fc59744fc66
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-05-10 13:08:28 +0100

    Foreach's Done

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -8,11 +8,38 @@ namespace IntCollections
         static void Main(string[] args)
         {
             //instanciar as coleções
-            List<int> list = new List<int>();
-            Stack<int> stack = new Stack<int>();
-            Queue<int> q = new Queue<int>();
-            HashSet<int> hash = new HashSet<int>();
+            List<int> list = new List<int>() {1, 10, -30, 10, -5};
+            Stack<int> stack = new Stack<int>(list) ;
+            Queue<int> q = new Queue<int>(list);
+            HashSet<int> hash = new HashSet<int>() {1, 10, -30, 10, -5};
             
+            Console.Write("List:");
+            foreach (int i in list)
+            {
+                Console.Write($" {i},");
+            }
+            Console.WriteLine("");
+
+            Console.Write("Stack:");
+            foreach (int i in stack)
+            {
+                Console.Write($" {i},");
+            }
+            Console.WriteLine("");
+
+            Console.Write("Queue:");
+            foreach (int i in q)
+            {
+                Console.Write($" {i},");
+            }
+            Console.WriteLine("");
+
+            Console.Write("HashSet:");
+            foreach (int i in hash)
+            {
+                Console.Write($" {i},");
+            }
+            Console.WriteLine("");            
         }
     }
 }
